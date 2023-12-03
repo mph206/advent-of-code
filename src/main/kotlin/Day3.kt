b7823d191719fdf0f3e4d0b3b0fb7689a561552b
mph206 <mpharrison206@gmail.com>
2023-12-03 19:07:17 +0000

    Day 3: tidy up

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -2,74 +2,73 @@ import java.io.File
 
 class Day3(file: File) {
     private val input = file.readLines()
-    private val asterisksWithNumbers = mutableMapOf<Coordinate, MutableList<Int>>()
 
-    fun part1(): Int {
-        val matrix = input.flatMapIndexed { lineIndex, line ->
-            line.mapIndexed { charIndex, char ->
-                when {
-                    char.isDigit() && (charIndex == 0 || !line[charIndex - 1].isDigit()) -> {
-                        val number = buildNumber(charIndex, line, lineIndex)
-                        val surrounding = buildSurroundingCoordinates(input, number.second)
-                        val surroundContainsSymbols = isAdjacentToSymbol(input, surrounding, number.first.toInt())
-                        number.first.toInt() to surroundContainsSymbols
-                    }
-                    else -> null
-                }
-            }.filterNotNull()
-        }
-        return matrix.filter { it.second }.sumOf { it.first }
-    }
-
-    fun part2(): Int {
-        val asterisks = input.flatMapIndexed { lineIndex, line -> line.mapIndexed { charIndex, char ->
-            if (char == '*') Coordinate(lineIndex, charIndex) else null
+    fun sumPartNumbers(): Int = input.flatMapIndexed { lineIndex, line ->
+        line.mapIndexed { charIndex, char ->
+            if (char.isDigit() && (charIndex == 0 || !line[charIndex - 1].isDigit())) {
+                val currentNumber = buildNumber(charIndex, line, lineIndex)
+                val surroundingCoordinates = buildSurroundingCoordinates(input, currentNumber.coordinates)
+                return@mapIndexed currentNumber.value to isAdjacentToSymbol(surroundingCoordinates)
             }
-        }.filterNotNull().forEach {
-            asterisksWithNumbers[it] = mutableListOf()
+            null
+        }.filterNotNull()
+    }.filter { it.second }.sumOf { it.first }
+
+    fun sumGearRatios(): Int {
+        val asteriskNumbers = mutableMapOf<Coordinate, MutableList<Int>>()
+        input.forEachIndexed { lineIndex, line ->
+            line.forEachIndexed { charIndex, char ->
+                if (char.isDigit() && (charIndex == 0 || !line[charIndex - 1].isDigit())) {
+                    val currentNumber = buildNumber(charIndex, line, lineIndex)
+                    val surroundingCoordinates = buildSurroundingCoordinates(input, currentNumber.coordinates)
+                    findAdjacentAsterisks(surroundingCoordinates).forEach {
+                        asteriskNumbers[it]?.add(currentNumber.value) ?: asteriskNumbers.put(it,
+                            mutableListOf(currentNumber.value))
+                    }
+                }
+            }
         }
-        part1()
-        return asterisksWithNumbers.filter { it.value.size > 1 }.map { it.value.reduce { acc, i -> acc * i } }.sum()
+        return asteriskNumbers.filter { it.value.size > 1 }.map { it.value.reduce { acc, i -> acc * i } }.sum()
     }
 
-    private fun buildNumber(charIndex: Int, line: String, lineIndex: Int): Pair<String, List<Coordinate>> {
+    private fun buildNumber(charIndex: Int, line: String, lineIndex: Int): NumberWithCoordinates {
         var index = charIndex
         var num = ""
-        val position = mutableListOf<Coordinate>()
+        val coordinates = mutableListOf<Coordinate>()
         while (line.length > index && line[index].isDigit()) {
             num += line[index]
-            position.add(Coordinate(lineIndex, index))
+            coordinates.add(Coordinate(lineIndex, index))
             index++
         }
-        return Pair(num, position)
+        return NumberWithCoordinates(num.toInt(), coordinates)
     }
 
-    private fun buildSurroundingCoordinates(input: List<String>, numberCoordinates: List<Coordinate>): List<Coordinate> {
-        val surrounds = mutableSetOf<Coordinate>()
-        val lineLength = input.first().length
-        numberCoordinates.forEach { coordinate ->
-            run {
-                surrounds.add(Coordinate(coordinate.y - 1, coordinate.x))
-                surrounds.add(Coordinate(coordinate.y + 1, coordinate.x))
-                surrounds.add(Coordinate(coordinate.y - 1, coordinate.x - 1))
-                surrounds.add(Coordinate(coordinate.y, coordinate.x - 1))
-                surrounds.add(Coordinate(coordinate.y + 1, coordinate.x - 1))
-                surrounds.add(Coordinate(coordinate.y - 1, coordinate.x + 1))
-                surrounds.add(Coordinate(coordinate.y, coordinate.x + 1))
-                surrounds.add(Coordinate(coordinate.y + 1, coordinate.x + 1))
+    private fun buildSurroundingCoordinates(input: List<String>, numberCoordinates: List<Coordinate>) =
+        numberCoordinates
+            .flatMap { coordinate ->
+                listOf(Coordinate(coordinate.y - 1, coordinate.x - 1),
+                    Coordinate(coordinate.y - 1, coordinate.x),
+                    Coordinate(coordinate.y - 1, coordinate.x + 1),
+                    Coordinate(coordinate.y, coordinate.x - 1),
+                    Coordinate(coordinate.y, coordinate.x + 1),
+                    Coordinate(coordinate.y + 1, coordinate.x - 1),
+                    Coordinate(coordinate.y + 1, coordinate.x),
+                    Coordinate(coordinate.y + 1, coordinate.x + 1))
             }
-        }
-        return surrounds.filter { it.x >= 0 && it.y >= 0 && it.x < lineLength && it.y < input.size }
-    }
+            .toSet()
+            .filter { it.x >= 0 && it.y >= 0 && it.x < input.first().length && it.y < input.size }
 
-    private fun isAdjacentToSymbol(input: List<String>, coordinates: List<Coordinate>, number: Int): Boolean {
-        coordinates.filter { input[it.y][it.x] == '*' }.forEach { coord -> asterisksWithNumbers[coord]?.add(number) }
-        return coordinates.any { !input[it.y][it.x].isDigit() && !input[it.y][it.x].isLetter() && input[it.y][it.x] != '.' }
-    }
+    private fun isAdjacentToSymbol(coordinates: List<Coordinate>): Boolean =
+        coordinates.any { !input[it.y][it.x].isDigit() && !input[it.y][it.x].isLetter() && input[it.y][it.x] != '.' }
+
+    private fun findAdjacentAsterisks(coordinates: List<Coordinate>): List<Coordinate> =
+        coordinates.filter { input[it.y][it.x] == '*' }
 }
+
+data class NumberWithCoordinates(val value: Int, val coordinates: List<Coordinate>)
 
 fun main() {
     val calculator = Day3(File("src/main/resources/day_3_input.txt"))
-    println(calculator.part1()) // 507214
-    println(calculator.part2()) // 72553319
+    println(calculator.sumPartNumbers()) // 507214
+    println(calculator.sumGearRatios()) // 72553319
 }
--- src/test/kotlin/Day3Test.kt
+++ src/test/kotlin/Day3Test.kt
@@ -5,12 +5,12 @@ import java.io.File
 class Day3Test {
     private val calculator = Day3(File("src/test/resources/day_3_test_input.txt"))
     @Test
-    fun `part 1`() {
-        assertEquals(4361, calculator.part1())
+    fun `Sums part numbers`() {
+        assertEquals(4361, calculator.sumPartNumbers())
     }
 
     @Test
-    fun `part 2`() {
-        assertEquals(467835, calculator.part2())
+    fun `Sums gear ratios`() {
+        assertEquals(467835, calculator.sumGearRatios())
     }
 }
