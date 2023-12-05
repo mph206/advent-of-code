85726ef21449eace774b20601dbd4261173340ff
mph206 <mpharrison206@gmail.com>
2023-12-05 18:36:42 +0000

    Day 4.1 - working solution

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,41 +1,35 @@
 import java.io.File
-import kotlin.math.pow
+
 
 class Day5(file: File) {
     private val input = file.readLines()
-    val seeds = input.first().substring(7).split(" ").mapNotNull { it.toLongOrNull() }
-    val seedToSoil = buildMap("seed", "soil")
-    val soilToFertilizer = buildMap("soil", "fertilizer")
-    val fertilizerToWater = buildMap("fertilizer", "water")
-    val waterToLight = buildMap("water", "light")
-    val lightToTemperature = buildMap("light", "temperature")
-    val temperatureToHumidity = buildMap("temperature", "humidity")
-    val humidityToLocation = buildMap("humidity", "location")
 
-    private fun buildMap(from: String, to: String): Map<Long, Long> {
-        val section = input.indexOfFirst { it.contains("$from-to-$to") }
-        val end = input.subList(section + 1, input.size - 1).takeWhile { it.isNotBlank() }
-        val map = mutableMapOf<Long, Long>()
-        end.forEach { line ->
+    private fun findDestinationNameAndValue(sourceName: String, sourceValue: Long): Pair<String?, Long> {
+        val sectionStart = input.indexOfFirst { it.contains("$sourceName-to") }
+        if (sectionStart < 0) return Pair(null, sourceValue)
+        val destinationName = input[sectionStart].split("$sourceName-to-", " ").getOrNull(1)
+        val row = input.subList(sectionStart + 1, input.size).takeWhile { it.isNotBlank() }
+        var value: Long? = null
+
+        row.forEach { line ->
             val (dest, start, range) = line.split(" ").mapNotNull { it.toLongOrNull() }
-            // TODO: too inefficient for full data. Could we just have the Map Key as a range? Or simplify some other way?
-            for (i in 0 until range) {
-                map[start + i] = dest + i
+            if (start < sourceValue && start + range >= sourceValue) {
+                value = sourceValue + dest - start
             }
         }
-        return map
+        return Pair(first = destinationName, second = value ?: sourceValue)
     }
 
     fun part1(): Long {
+        val seeds = input.first().substring(7).split(" ").mapNotNull { it.toLongOrNull() }
         val locations = seeds.map { seed ->
-            val soil = seedToSoil.getOrDefault(seed, seed)
-            val fertilizer = soilToFertilizer.getOrDefault(soil, soil)
-            val water = fertilizerToWater.getOrDefault(fertilizer, fertilizer)
-            val light = waterToLight.getOrDefault(water, water)
-            val temperature = lightToTemperature.getOrDefault(light, light)
-            val humidity = temperatureToHumidity.getOrDefault(temperature, temperature)
-            val location = humidityToLocation.getOrDefault(humidity, humidity)
-            location
+            var (destinationName, destinationValue) = findDestinationNameAndValue("seed", seed)
+            while (destinationName != null) {
+                val output = findDestinationNameAndValue(destinationName, destinationValue)
+                destinationName = output.first
+                destinationValue = output.second
+            }
+            destinationValue
         }
         return locations.minOrNull() ?: throw Exception("No minimum found")
     }
@@ -47,6 +41,6 @@ class Day5(file: File) {
 
 fun main() {
     val calculator = Day5(File("src/main/resources/day_5_input.txt"))
-    println(calculator.part1()) //
+    println(calculator.part1()) // 196167384
     println(calculator.part2()) //
 }
