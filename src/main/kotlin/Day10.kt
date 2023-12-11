225e7eca029799e4d164be1e1da3e181bd29ac9a
mph206 <mpharrison206@gmail.com>
2023-12-11 21:53:19 +0000

    10.2

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -6,9 +6,52 @@ class Day10(file: File) {
     private val input = file.readLines()
 
     fun part1(): Int {
-        val startingPositionY = input.indexOfFirst { it.contains('S') }
-        val startingPositionX = input[startingPositionY].indexOf('S')
-        val startingPosition = Coordinate(startingPositionY, startingPositionX)
+        val visited = buildLoop()
+        return (floor(visited.toSet().count().toDouble()) / 2).toInt()
+    }
+
+    fun part2(): Int {
+        val visited = buildLoop()
+        val corners = listOf('F', 'J', 'L', '7')
+        val otherSidePairs = mapOf('F' to 'J', 'L' to '7', '7' to 'L', 'J' to 'F')
+        val sameSidePairs = mapOf('L' to 'J', 'F' to '7', 'J' to 'L', '7' to 'F')
+        val startPosition = findStartPosition()
+        val startChar = startPosition.findCharFromExits(startPosition.findStartExits())
+        return input.mapIndexed { lineIndex, line ->
+            var isInsideLoop = false
+            val cornersToProcess = mutableListOf<Char>()
+            var groundCount = 0
+            for ((charIndex, char) in line.withIndex()) {
+                val actualChar = if (char == 'S') startChar else char
+                when {
+                    Coordinate(lineIndex, charIndex) !in visited && isInsideLoop -> groundCount++
+                    Coordinate(lineIndex, charIndex) in visited -> {
+                        when (actualChar) {
+                            in corners -> {
+                                val otherSidePair = otherSidePairs[actualChar]
+                                val sameSidePair = sameSidePairs[actualChar]
+                                when {
+                                    otherSidePair != null && cornersToProcess.contains(otherSidePair) -> {
+                                        cornersToProcess.remove(otherSidePair)
+                                        isInsideLoop = !isInsideLoop
+                                    }
+                                    sameSidePair != null && cornersToProcess.contains(sameSidePair) -> {
+                                        cornersToProcess.remove(sameSidePair)
+                                    }
+                                    else -> cornersToProcess.add(actualChar)
+                                }
+                            }
+                            '|' -> isInsideLoop = !isInsideLoop
+                        }
+                    }
+                }
+            }
+            groundCount
+        }.sum()
+    }
+
+    private fun buildLoop(): List<Coordinate> {
+        val startingPosition = findStartPosition()
         var nextPosition = startingPosition.findStartExits().last()
         val visited = mutableListOf(startingPosition)
         var hasNext = true
@@ -19,10 +62,14 @@ class Day10(file: File) {
                 else -> nextPosition = next
             }
         }
-        return (floor(visited.toSet().count().toDouble()) / 2).toInt()
+        return visited
     }
 
-    fun part2() = 1
+    private fun findStartPosition(): Coordinate {
+        val startingPositionY = input.indexOfFirst { it.contains('S') }
+        val startingPositionX = input[startingPositionY].indexOf('S')
+        return Coordinate(startingPositionY, startingPositionX)
+    }
 
     private fun Coordinate.findNext(previous: Coordinate) = this.findExits()?.firstOrNull { it != previous }
 
@@ -45,7 +92,14 @@ class Day10(file: File) {
                 Coordinate(this.y + it.y, this.x + it.x)
             }
             .filter { it.y >= 0 && it.y < input.size && it.x >= 0 && it.x < input.first().length }
-        return surrChars.associateWith { it.findExits() }.filter { coord -> coord.value != null && coord.value?.any { it == this } ?: throw Exception("Can't find start") }.keys.toList()
+        return surrChars.associateWith { it.findExits() }
+            .filter { coord -> coord.value != null && coord.value?.any { it == this } ?: throw Exception("Can't find start") }.keys.toList()
+    }
+
+    private fun Coordinate.findCharFromExits(exits: List<Coordinate>): Char {
+        if (exits.size != 2) throw Exception("Size must be 2 to map to Char")
+        val relativeExits = exits.map { Coordinate(it.y - this.y, it.x - this.x) }
+        return possibleExits.filterValues { it.containsAll(relativeExits) }.keys.first()
     }
 
     private val possibleExits = mapOf(
@@ -56,12 +110,11 @@ class Day10(file: File) {
         '7' to listOf(Coordinate(1, 0), Coordinate(0, -1)),
         'F' to listOf(Coordinate(1, 0), Coordinate(0, 1))
     )
-
 }
 
 
 fun main() {
     val calculator = Day10(File("src/main/resources/day_10_input.txt"))
     println(calculator.part1()) // 6806
-    println(calculator.part2()) //
+    println(calculator.part2()) // 449
 }
--- src/test/kotlin/Day10Test.kt
+++ src/test/kotlin/Day10Test.kt
@@ -16,8 +16,27 @@ class Day10Test {
         assertEquals(8, calculator.part1())
     }
 
-//    @Test
-//    fun `Part 2`() {
-//        assertEquals(2, calculator.part2())
-//    }
+    @Test
+    fun `Part 2 - example 1`() {
+        val calculator = Day10(File("src/test/resources/day_10_test_input(3).txt"))
+        assertEquals(4, calculator.part2())
+    }
+
+    @Test
+    fun `Part 2 - example 2`() {
+        val calculator = Day10(File("src/test/resources/day_10_test_input(4).txt"))
+        assertEquals(4, calculator.part2())
+    }
+
+    @Test
+    fun `Part 2 - example 3`() {
+        val calculator = Day10(File("src/test/resources/day_10_test_input(5).txt"))
+        assertEquals(8, calculator.part2())
+    }
+
+    @Test
+    fun `Part 2 - example 4`() {
+        val calculator = Day10(File("src/test/resources/day_10_test_input(6).txt"))
+        assertEquals(10, calculator.part2())
+    }
 }
