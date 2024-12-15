3aef35352ec5d8cbb633ecf66d5edfe154f552d7
Martin Harrison <mpharrison206@gmail.com>
2024-12-15 15:21:42 +0000

    remove previously committed test input

--- 2023/settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-
-rootProject.name = "AdventOfCode2023"
-
--- 2024/utils.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from 'node:fs';
-
-function parseFile(file: string) {
-    return fs.readFileSync(file).toString('utf-8');
-}
-
-export {
-    parseFile
-};
