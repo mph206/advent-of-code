8861e48181c613b2f0c962ba1184c3b54a5542dd
Martin Harrison <mpharrison206@gmail.com>
2025-01-01 14:39:20 +0000

    11.2

--- 2024/day_11/solution.ts
+++ 2024/day_11/solution.ts
@@ -1,33 +1,42 @@
 import { parseFile } from '../utils.ts';
 
 function transformStones(input: string, blinks: number): number {
-    let stones = input.split(" ").map(it => Number(it));
+    let stoneMap = new Map<number, number>();
+    input.split(" ").forEach(it => {
+        const currentValue = stoneMap.get(Number(it));
+        stoneMap.set(Number(it), currentValue ? currentValue + 1 : 1);
+    });
     for (let i = 0; i < blinks; i++) {
-        stones = performBlink(stones)
+        stoneMap = performBlink(stoneMap)
     }
-    return stones.length;
+    return Array.from(stoneMap.values()).reduce((prev, curr) => prev + curr, 0);
 }
 
-function performBlink(stones: number[]): number[] {
-    const newStones: number[] = [];
-    stones.forEach(stone => {
+function performBlink(stoneMap: Map<number, number>): Map<number, number> {
+    const newStoneMap = new Map<number, number>();
+    stoneMap.forEach((count, stone) => {
         if (stone === 0) {
-            newStones.push(1)
+            const existingCount = newStoneMap.get(1);
+            newStoneMap.set(1, existingCount ? existingCount + count : count)
         } else if (stone.toString().length % 2 === 0) {
             const stoneString = stone.toString();
             const midwayPoint = stoneString.length / 2;
+            const stoneOne = Number(stoneString.slice(0, midwayPoint));
             const stoneTwo = Number(stoneString.slice(midwayPoint)); // trim leading 0s
-            newStones.push(Number(stoneString.slice(0, midwayPoint)), stoneTwo);
+
+            const stoneOneValue = newStoneMap.get(stoneOne);
+            newStoneMap.set(stoneOne, stoneOneValue ? stoneOneValue + count : count);
+
+            const stoneTwoValue = newStoneMap.get(stoneTwo);
+            newStoneMap.set(stoneTwo, stoneTwoValue ? stoneTwoValue + count : count);
         } else {
-            newStones.push((Number(stone) * 2024))
+            const newValue = stone * 2024;
+            const existingCount = newStoneMap.get(newValue);
+            newStoneMap.set(newValue, existingCount ? existingCount + count : count);
         }
     });
-    return newStones;
+    return newStoneMap;
 }
 
 console.log(transformStones(parseFile("day_11/input.txt"), 25)); // 209412
-console.log(transformStones(parseFile("day_11/input.txt"), 75)); // 
-
-export {
-    transformStones
-}
+console.log(transformStones(parseFile("day_11/input.txt"), 75)); // 248967696501656
